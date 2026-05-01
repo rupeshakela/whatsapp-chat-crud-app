50adbb79edf9a72f6e396d4488c1fcae2808c867
Rupesh Kumar <rayr51488@gmail.com>
2026-05-01 07:28:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ main().then(()=>{
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect(process.env.MONGO_URL);
 }
 
 
